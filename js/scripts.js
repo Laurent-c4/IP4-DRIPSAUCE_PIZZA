a1e2ebd6378c75aec30cc92be3b601a6a78971c6
Laurent Juma <lcbillax.lj@gmail.com>
2019-08-31 17:24:34 +0300

    Create pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+//UI Logic
 $(document).ready(function () {
     $("form").submit(function () {
         event.preventDefault();
         location.href = "../checkout.html";
     });
 });
+
+//Business Logic
+function Pizza (price,crust,topping) {
+    this.price=price;
+    this.topping=topping;
+    this.crust=crust;
+}
