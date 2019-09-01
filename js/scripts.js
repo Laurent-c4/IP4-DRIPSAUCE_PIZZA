3f4466bdc7af3b076ad1b225149c0f6efc381b8f
Laurent Juma <lcbillax.lj@gmail.com>
2019-09-01 13:27:34 +0300

    Add more pizza constructors to menu, for different types of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,93 @@ var inputtedSize;
 var inputtedTopping;
 var inputtedCrust;
 var pizzaPrice;
+var pizzaType;
 
 //BUSINESS LOGIC
-//Pizza Object
-function Pizza(size, crust, topping) {
+
+//Create Pizza Objects
+//BBQ Chicken Pizza
+function bbqChickenPizza(size, crust, topping) {
     this.size = size;
     this.topping = topping;
     this.crust = crust;
+    this.type = "bbq_c";
 }
 
-//Price for Size
+//BBQ Steak Pizza
+function bbqSteakPizza(size, crust, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.type = "bbq_s";
+}
+
+//Chicken Tikka Pizza
+function chickenTikkaPizza(size, crust, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.type = "chk_tk";
+}
+
+//Hawaiian Chicken Pizza
+function hawaiianPizza(size, crust, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.type = "haw";
+}
+
+//Margherita Pizza
+function margheritaPizza(size, crust, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.type = "margh";
+}
+
+//Meatzza Pizza
+function meatzzaPizza(size, crust, topping) {
+    this.size = size;
+    this.topping = topping;
+    this.crust = crust;
+    this.type = "mtz";
+}
+
+
+//Default Pizza Price (Small Pizza)
+var pizzaTypePriceF = function () {
+    var pizzaTypePrice;
+    if (pizzaType === "bbq_c") {
+        pizzaTypePrice = 500;
+    } else if (pizzaType === "bbq_s") {
+        pizzaTypePrice = 600;
+    } else if (pizzaType === "chk_tk") {
+        pizzaTypePrice = 700;
+    } else if (pizzaType === "haw") {
+        pizzaTypePrice = 800;
+    } else if (pizzaType === "margh") {
+        pizzaTypePrice = 900;
+    } else if (pizzaType === "mtz") {
+        pizzaTypePrice = 1000;
+    }
+    return pizzaTypePrice;
+}
+
+//Additional Price Based on Size
 var pizzaSizePriceF = function () {
     var pizzaSizePrice;
     if (inputtedSize === "large") {
-        pizzaSizePrice = 300;
+        pizzaSizePrice = 400;
     } else if (inputtedSize === "medium") {
         pizzaSizePrice = 200;
     } else if (inputtedSize === "small") {
-        pizzaSizePrice = 100;
+        pizzaSizePrice = 0;
     }
     return pizzaSizePrice;
 }
 
-//Price for Crust
+//Additional Price Based on Crust
 var pizzaCrustPriceF = function () {
     var pizzaCrustPrice;
     if (inputtedCrust == "classic_crust") {
@@ -40,7 +104,7 @@ var pizzaCrustPriceF = function () {
 }
 
 
-//Price for Toppings depending on Pizza Size
+//Additional Price Based on Toppings (Toppings Pricing also relies on Pizza Size)
 var pizzaToppingPriceF = function () {
     var pizzaToppingPrice;
     if (inputtedSize === "large" && inputtedTopping === "pepperoni") {
@@ -72,8 +136,8 @@ var pizzaToppingPriceF = function () {
 }
 
 var pizzaPriceF = function () {
-pizzaPrice = pizzaSizePriceF() + pizzaToppingPriceF() + pizzaCrustPriceF();
-return pizzaPrice;
+    pizzaPrice = pizzaSizePriceF() + pizzaToppingPriceF() + pizzaCrustPriceF() + pizzaTypePriceF();
+    return pizzaPrice;
 }
 
 //UI LOGIC
@@ -81,70 +145,76 @@ $(document).ready(function () {
 
 
     //Get User Input for pizza preference
-    $(".bbq_c").on('change',function () {
+    $(".bbq_c").on('change', function () {
         inputtedSize = $("#bbq_c-size").val();
         inputtedTopping = $("#bbq_c-toppings").val();
         inputtedCrust = $("#bbq_c-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        bbqChickenOrder = new bbqChickenPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = bbqChickenOrder.type;
         $("#bbq_c-price").remove();
         $("#bbq_c-card").append("<div id='bbq_c-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
-    $(".bbq_s").on('change',function () {
+    $(".bbq_s").on('change', function () {
         inputtedSize = $("#bbq_s-size").val();
         inputtedTopping = $("#bbq_s-toppings").val();
         inputtedCrust = $("#bbq_s-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        bbqSteakOrder = new bbqSteakPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = bbqSteakOrder.type;
         $("#bbq_s-price").remove();
         $("#bbq_s-card").append("<div id='bbq_s-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
-    $(".chk-tk").on('change',function () {
+    $(".chk-tk").on('change', function () {
         inputtedSize = $("#chk_tk-size").val();
         inputtedTopping = $("#chk_tk-toppings").val();
         inputtedCrust = $("#chk_tk-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        chickenTikkaOrder = new chickenTikkaPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = chickenTikkaOrder.type;
         $("#chk_tk-price").remove();
         $("#chk_tk-card").append("<div id='chk_tk-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
-    $(".haw").on('change',function () {
+    $(".haw").on('change', function () {
         inputtedSize = $("#haw-size").val();
         inputtedTopping = $("#haw-toppings").val();
         inputtedCrust = $("#haw-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        hawaiianOrder = new hawaiianPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = hawaiianOrder.type;
         $("#haw-price").remove();
         $("#haw-card").append("<div id='haw-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
-    $(".margh").on('change',function () {
+    $(".margh").on('change', function () {
         inputtedSize = $("#margh-size").val();
         inputtedTopping = $("#margh-toppings").val();
         inputtedCrust = $("#margh-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        margheritaOrder = new margheritaPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = margheritaOrder.type;
         $("#margh-price").remove();
         $("#margh-card").append("<div id='margh-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
-    $(".mtz").on('change',function () {
+    $(".mtz").on('change', function () {
         inputtedSize = $("#mtz-size").val();
         inputtedTopping = $("#mtz-toppings").val();
         inputtedCrust = $("#mtz-crust").val();
-        pizzaOrder = new Pizza(inputtedSize, inputtedCrust, inputtedTopping);
+        meatzzaOrder = new meatzzaPizza(inputtedSize, inputtedCrust, inputtedTopping);
+        pizzaType = meatzzaOrder.type;
         $("#mtz-price").remove();
         $("#mtz-card").append("<div id='mtz-price'>" +
-                  "<p>"+pizzaPriceF()+"</p>"+              
-        "</div>");
+            "<p>" + pizzaPriceF() + "</p>" +
+            "</div>");
     });
 
 
